65f1fa09d7bf6e9b8af24c28d55f6510ca5fbb3b
James A <jaubin9422@gmail.com>
2021-08-25 13:07:40 -0500

    pain

---
+++ mod.cpp
@@ -0,0 +1,20 @@
+name		 = "AppTac Uniforms";								
+picture 	 = "\apptac_uniforms\UI\spoons_ca.paa"; 	
+logoSmall	 = "\apptac_uniforms\UI\spoons_ca.paa";	
+logo		 = "\apptac_uniforms\UI\spoons_ca.paa";			
+logoOver	 = "\apptac_uniforms\UI\spoons_ca.paa";		
+action		 = "https://appliedtacticalgaming.com/";			
+tooltipOwned = "Turn back now";								
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+// Overview text, displayed from the extension menu
+overview = "Questionable vanilla retextures for use w/ my friends";
+hideName	= 0;	// Hide the extension name
+hidePicture	= 0;	// Hide the extension menu
